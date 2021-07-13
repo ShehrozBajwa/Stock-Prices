d96c0325a0b53be3472b9341772f1dd5f891f416
ShehrozBajwa <64991796+ShehrozBajwa@users.noreply.github.com>
2021-07-13 16:34:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import discord, asyncpg
 from discord.ext import tasks, commands
+from bs4 import BeautifulSoup, BeautifulStoneSoup
+import requests
 
 channel_id = '<@180804112567369728>'
 client = discord.Client()
 bot_token = 'ODY0MTg1MTcyNzkwMjE0Njk2.YOxxKA.BqqPjCQXf607yV2nXVOZlWfGSUE'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+ticker = ['GME', 'BB', 'CLOV']
+
+
 @client.event
 async def on_ready():
     print('Bot Ready.')
 
 @client.event
 async def on_message(message):
-    print('Bot Ready.')
+        if message.author == client.user:
+            return
+        
+        if message.content.startswith('!view'):
+            await message.channel.send(view())
+    
+def view():
+    stockPriceOutput  = ""
+    for i in range(0, len(ticker)):
+        url = 'https://finance.yahoo.com/quote/%s' % ticker[i]
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        stockData = soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('span')
+        currPrice = stockData[0].text.strip()
+        changeInPrice = stockData[1].text.strip()
+        percent = changeInPrice.split('(', 1)[1].split(')')[0]
+        percent = percent.strip('%-+')
+        percent = float(percent)
+        stockPriceOutput += "%s:\n    Current Price: %s \n    Change in Price: %s \n" % (ticker[i], currPrice, changeInPrice)
+    return stockPriceOutput
 
 
 client.run(bot_token)
